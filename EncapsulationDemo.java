8c39bd3aef3342878eb37aa8bebb3b9b9d0d59f3
abhijitramesh <abhijitramesh2k@gmail.com>
2018-10-28 14:03:26 +0530

    Encapsulation

---
+++ EncapsulationDemo.java
@@ -0,0 +1,32 @@
+//Encapsulation -> Binding data with methods
+
+class Student{
+	private int rollno;
+	private String name;
+	
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	//Getters and Setters
+
+}
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.setRollno(1);
+		s1.setName("abhijit");
+		System.out.println(s1.getRollno());
+		System.out.println(s1.getName());
+	}
+
+}
