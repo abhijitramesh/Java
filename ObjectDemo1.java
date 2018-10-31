1c8b0c117c4ad89c467a93c3bb962b3b30482843
abhijitramesh <43090559+abhijitramesh@users.noreply.github.com>
2018-10-31 18:56:01 +0530

    Update
    commented

--- ObjectDemo1.java
+++ ObjectDemo1.java
@@ -1,38 +1,63 @@
-import java.util.*;//importing util class to use scanner class
+@author		Abhijit Ramesh
+import java.util.*;/**importing the java.util package 
+		    *to use the scanner class to take the input from the user
+		    */
 class Calculator{
-	int num1;
-	int num2;
+/** This class is created to take three values and perform four methods 
+*adding subtracting multiplying and dividing
+@param number1 This is the first parameter to Calculator class
+@param number2 This is the second parameter to Calculator class
+@param result This is the third parameter to Calculator class
+*/
+	int number1;
+	int number2;
 	int result;
 
-	public void add(){//method to perfome additon
-		result = num1 + num2 ;
+	public void add(){
+	/**this method is used to add two integers.
+	*this is to explain how objects usues methods to perform certain steps.
+	*this is bacially a behaviour showed by the object
+	*/
+		result = number1 + number2 ;
 
 	}
-	public void subtract(){//method to perfome subtraction
+	public void subtract(){
+	/**this method is used to subtract two integers.
+	*this is to explain how objects usues methods to perform certain steps.
+	*this is bacially a behaviour showed by the object
+	*/
 		result = num1 - num2;
 			}
-	public void multiply(){//method to perfome multiplication
+	public void multiply(){
+	/**this method is used to multiply two integers.
+	*this is to explain how objects usues methods to perform certain steps.
+	*this is bacially a behaviour showed by the object
+	*/
 		result = num1 * num2;
 	}
-	public void divide(){//method to perfome division
+	public void divide(){
+	/**this method is used to divide two integers.
+	*this is to explain how objects usues methods to perform certain steps.
+	*this is bacially a behaviour showed by the object
+	*/
 		result = num1/num2;
 	}
 }
-class ObjectDemo1{//class for main
-	public static void main(String[] arg){//starting main
-	Calculator calculator = new Calculator();//initialising object ob
-	Scanner scanner = new Scanner(System.in);//importing scanner class and initialising it to sc
-	System.out.println("enter two numbers");//asking user for two values 
-	calculator.num1 = scanner.nextInt();//initalising value to int num1 on object num1
-	calculator.num2 = scanner.nextInt();//initialisng value to int num2 on object num2
-	calculator.add();//performing addition
-	System.out.println("the sum of the numbers = "+calculator.result);//printing the result
-	calculator.subtract();//performing subtraction
-	System.out.println("the difference of the numbers = "+calculator.result);//printing the result
-	calculator.multiply();//performing multiplication
-	System.out.println("the product of the numbers = "+calculator.result);//printing the result
-	calculator.divide();//performing division
-	System.out.println("the quotient of the numbers = "+calculator.result);//printing the result
+class ObjectDemo1{
+	public static void main(String[] arg){
+	Calculator calculator = new Calculator();
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("enter two numbers");
+	calculator.number1 = scanner.nextInt();
+	calculator.number2 = scanner.nextInt();
+	calculator.add();
+	System.out.println("the sum of the numbers = "+calculator.result);
+	calculator.subtract();
+	System.out.println("the difference of the numbers = "+calculator.result);
+	calculator.multiply();
+	System.out.println("the product of the numbers = "+calculator.result);
+	calculator.divide();
+	System.out.println("the quotient of the numbers = "+calculator.result);
 }
 }
 
