6869f6abb897e6ebc71ec82993147880ea621193
abhijitramesh <43090559+abhijitramesh@users.noreply.github.com>
2018-10-31 18:26:30 +0530

    Update
    Changed some identifiers

--- ObjectDemo1.java
+++ ObjectDemo1.java
@@ -1,8 +1,8 @@
 import java.util.*;//importing util class to use scanner class
-class Calc{//class for initialising methods and variables
-	int num1;//1st variable
-	int num2;//2nd variable
-	int result;//3rd variable
+class Calculator{
+	int num1;
+	int num2;
+	int result;
 
 	public void add(){//method to perfome additon
 		result = num1 + num2 ;
@@ -20,19 +20,19 @@ class Calc{//class for initialising methods and variables
 }
 class ObjectDemo1{//class for main
 	public static void main(String[] arg){//starting main
-	Calc ob = new Calc();//initialising object ob
-	Scanner sc = new Scanner(System.in);//importing scanner class and initialising it to sc
+	Calcalculator calculator = new Calcalculator();//initialising object ob
+	Scanner scanner = new Scanner(System.in);//importing scanner class and initialising it to sc
 	System.out.println("enter two numbers");//asking user for two values 
-	ob.num1 = sc.nextInt();//initalising value to int num1 on object num1
-	ob.num2 = sc.nextInt();//initialisng value to int num2 on object num2
-	ob.add();//performing addition
-	System.out.println("the sum of the numbers = "+ob.result);//printing the result
-	ob.subtract();//performing subtraction
-	System.out.println("the difference of the numbers = "+ob.result);//printing the result
-	ob.multiply();//performing multiplication
-	System.out.println("the product of the numbers = "+ob.result);//printing the result
-	ob.divide();//performing division
-	System.out.println("the quotient of the numbers = "+ob.result);//printing the result
+	calculator.num1 = scanner.nextInt();//initalising value to int num1 on object num1
+	calculator.num2 = scanner.nextInt();//initialisng value to int num2 on object num2
+	calculator.add();//performing addition
+	System.out.println("the sum of the numbers = "+calculator.result);//printing the result
+	calculator.subtract();//performing subtraction
+	System.out.println("the difference of the numbers = "+calculator.result);//printing the result
+	calucator.multiply();//performing multiplication
+	System.out.println("the product of the numbers = "+calculator.result);//printing the result
+	calculator.divide();//performing division
+	System.out.println("the quotient of the numbers = "+calculator.result);//printing the result
 }
 }
 
