e2790a72fa94ca8d04251a787d4a8398c93f7dce
abhijitramesh <abhijitramesh2k@gmail.com>
2018-11-15 17:04:41 +0530

    packageDemo

---
+++ com/abhijitjithu/test/one/Lambo.java
@@ -0,0 +1,11 @@
+package com.abhijitjithu.test.one;
+import com.abhijitjithu.test.two.Cars;
+
+public class Lambo {
+
+	public static void main(String[] args) {
+		Cars lambo = new Cars();
+		lambo.model = "Hurracn";
+	}
+
+}
---
+++ com/abhijitjithu/test/two/Cars.java
@@ -0,0 +1,7 @@
+package com.abhijitjithu.test.two;
+
+public class Cars {
+public String model = "Lamborghini";
+
+	
+}
