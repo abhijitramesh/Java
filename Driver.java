930c627bb9bbbce45b970d8c5d2d12fc1ecccb95
Abhijit Ramesh <abhijitramesh2k@gmail.com>
2019-06-24 09:59:45 +0530

    Lab work Encapsulation Exercise

---
+++ Driver.java
@@ -0,0 +1,29 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Point p = new Point();
+		Point p1 = new Point();
+		p.setX(2);
+		p.setY(5);
+		p.print();
+		p1.setX(10);
+		p1.setY(5);
+		
+		if(p.getX() == p1.getX()) {
+			System.out.println("Same X");
+		}
+		else if(p.getY() == p1.getY()) {
+			System.out.println("Same y");
+		}
+		if(p == p1) {
+			System.out.println("Same");
+		}
+		else {
+			System.out.println("Different");
+
+		}
+		
+	}
+
+}
---
+++ Point.java
@@ -0,0 +1,23 @@
+
+public class Point {
+
+private int x;
+private int y;
+
+public void setX(int x) {
+	this.x=x;
+}
+public void setY(int y) {
+	this.y=y;
+}
+public int getX() {
+	return this.x;
+}
+public int getY() {
+	return this.y;
+}
+public void print() {
+	System.out.println("x ="+this.x+"\ny = "+this.y);
+}
+
+}
